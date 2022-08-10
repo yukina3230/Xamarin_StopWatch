69fba407309e01bf95bb12b8bdf0975883a7329c
Yukina <thienphuc3230@gmail.com>
2022-08-10 10:58:05 +0700

    Add project files.

---
+++ Xamarin_StopWatch/Xamarin_StopWatch/Models/DataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_StopWatch.Models
+{
+    public class DataModel
+    {
+        public string Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string ReasonId { get; set; }
+    }
+}
